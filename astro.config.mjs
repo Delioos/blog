d5c9f2c322cd45f9b4e39b18f1bdce32ad775993
Delioos <htz_j@protonmail.com>
2024-09-06 12:25:09 +0700

    meow vercel push

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://delioos.xyz",
+  //site: 'https://delioos.xyz',
+  //site: 'https://delioos.github.io',
+  base: 'blog',
 });
