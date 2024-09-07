7017e243b502a39839ef0add49f6e68881082275
Delioos <htz_j@protonmail.com>
2024-09-07 18:24:53 +0700

    fuck vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
   integrations: [tailwind(), mdx()],
-site: 'https://www.delioos.xyz'
+	site: 'https://www.delioos.xyz'
 });
 
 
