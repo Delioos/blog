9d5c9b13ceec799aeee7f4286c568cc326904f27
Delioos <htz_j@protonmail.com>
2024-09-07 18:02:51 +0700

    feat: vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: vercel(
+		{
+			webAnalytics: { enable: true}
+		}
+	),
   integrations: [tailwind(), mdx()],
-  //site: 'https://delioos.xyz',
+  site: 'https://delioos.xyz',
   //site: 'https://delioos.github.io',
   base: '/'
 });
