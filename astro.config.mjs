1b6a6042cfe6e542fd995130b1910e2106f3e3e0
Delioos <htz_j@protonmail.com>
2025-01-22 12:19:59 +0100

    fix: try to make vercel happy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import icon from "astro-icon";
 
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon()],
 	site: 'https://www.delioos.xyz',
   output: 'server',
-  adapter: node({
-    mode: 'middleware'
-  }),
+  adapter: vercel(),
 });
 
 
