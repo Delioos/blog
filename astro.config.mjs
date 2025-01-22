dc815047e71d0371260651399c1c85a6102d276b
Delioos <htz_j@protonmail.com>
2025-01-22 12:16:53 +0100

    deps: bump package.json + trick icon into asset for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import node from '@astrojs/node';
+import icon from "astro-icon";
 
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), icon()],
 	site: 'https://www.delioos.xyz',
   output: 'server',
   adapter: node({
